e7da2b389e49d17123083c9df3dcb3009b982462
fosmjo <imefangjie@gmail.com>
2023-04-04 11:36:33 +0800

    add gonew

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-pod=$(kubectl --context prod.k8s --namespace prod get po|grep "$1"|grep Running|head -n 1|awk '{print $1}')
+pod=$(kubectl --context prod22 --namespace prod get po|grep "$1"|grep Running|head -n 1|awk '{print $1}')
 shift
-cmd="kubectl --context prod.k8s --namespace prod exec -it $pod  -- env LANG=C.UTF-8 $@"
+cmd="kubectl --context prod22 --namespace prod exec -it $pod  -- env LANG=C.UTF-8 $@"
 echo "$cmd"
 eval "$cmd"
