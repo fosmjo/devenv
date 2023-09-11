ab8fa4ea2100c62ccde6accfa9653a83017795d3
fosmjo <imefangjie@gmail.com>
2023-09-11 18:26:28 +0800

    add clash mixin config in JavaScript

---
+++ clash/mixin.js
@@ -0,0 +1,18 @@
+module.exports.parse = async (
+  { content, name, url },
+  { axios, yaml, notify }
+) => {
+
+  const extraRules = [
+    "DOMAIN-SUFFIX,gravatar.com,代理",
+    "DOMAIN-SUFFIX,deepl.com,代理",
+    "DOMAIN-SUFFIX,poe.com,代理",
+    "DOMAIN-SUFFIX,formulae.brew.sh,代理",
+  ];
+
+  return { 
+    ...content, 
+    rules : [...extraRules, ...content.rules, ]
+  };
+};
+
