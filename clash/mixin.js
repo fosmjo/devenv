f8e0a8f2950d4e24f982186e5efb812c1b09028d
fosmjo <imefangjie@gmail.com>
2023-09-12 18:24:50 +0800

    update clash/mixin.js

--- clash/mixin.js
+++ clash/mixin.js
@@ -8,6 +8,12 @@ module.exports.parse = async (
     "DOMAIN-SUFFIX,deepl.com,代理",
     "DOMAIN-SUFFIX,poe.com,代理",
     "DOMAIN-SUFFIX,brew.sh,代理",
+    "DOMAIN-SUFFIX,ghcr.io,代理",
+    "DOMAIN-SUFFIX,configuration.ls.apple.com,代理",
+    "DOMAIN-SUFFIX,microsoft.com,代理",
+    "DOMAIN-SUFFIX,outlook.com,代理",
+    "DOMAIN-SUFFIX,visualstudio.com,代理",
+    "IP-CIDR,47.97.214.228/32,代理", // maybe github
   ];
 
   return {
